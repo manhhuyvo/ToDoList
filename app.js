1413de25d4647a66c13b3e4cda5d9d198562547c
Manh Huy Vo <manhhuyvo@gmail.com>
2022-09-11 21:16:18 +1000

    Deploying the project with Heroku

--- app.js
+++ app.js
@@ -153,9 +153,14 @@ function updateItems(newContent, description){ // UPDATE
 }
 /* CRUD operations */
 
-/* Open the server to listen on port 3000... */
-app.listen("3000",function(){
-    console.log("Server is running on port 3000...");
+/* Open the server to listen on port with Heroku or localhost 3000... */
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port,function(){
+    console.log("Server has started successfully...");
 });
 /* Open the server to listen on port 3000... */
 
